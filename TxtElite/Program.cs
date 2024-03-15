2cd0bd878f68cea536ec55565edb9b7e76aca590
space <o.catinas@gmail.com>
2024-03-15 19:42:47 +0000

    Setting up

---
+++ TxtElite/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TxtElite
+{
+    class Program
+    {
+        public const int True = -1;
+        public const int False = 0;
+
+        
+        public struct FastSeedType
+        {
+            public byte a;
+            public byte b;
+            public byte c;
+            public byte d;
+        }
+
+        static void Main(string[] args)
+        {
+            var maxlen = 20;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
